1b243d960e8b3ee53072f2488c951759e6403220
Lars Levie <larslevie@gmail.com>
2021-01-10 09:13:31 -0600

    Try to read Vault token from $VAULT_TOKEN_PATH

--- internal/providers/hashicorp_vault.go
+++ internal/providers/hashicorp_vault.go
@@ -26,12 +26,43 @@ func (p *HashicorpVaultProvider) GetKVSecrets(path string) (map[string]string, e
 }
 
 func NewHashicorpVaultProvider() (SecretsProvider, error) {
+	vaultToken, err := getToken()
+	if err != nil {
+		return nil, err
+	}
+
 	client, err := api.NewClient(nil)
 	if err != nil {
 		return nil, err
 	}
 
+	client.SetToken(vaultToken)
+
 	return &HashicorpVaultProvider{Client: client.Logical()}, err
+}
+
+func getToken() (string, error) {
+	t, exists := os.LookupEnv("VAULT_TOKEN")
+	if !exists {
+		tokenPath, exists := os.LookupEnv("VAULT_TOKEN_PATH")
+		if exists == false {
+			return "", errors.New("$VAULT_TOKEN unset and $VAULT_TOKEN_PATH unset")
+		}
+
+		tBytes, err := ioutil.ReadFile(tokenPath)
+		if err != nil {
+			fmt.Println("$VAULT_TOKEN_PATH set, but failed to read file")
+			return "", err
+		}
+
+		t = strings.TrimSpace(string(tBytes))
+		if len(t) == 0 {
+			fmt.Println("$VAULT_TOKEN_PATH set, but file is empty")
+			return "", err
+		}
+	}
+
+	return t, nil
 }
 
 func getKVSecret(name string, f func(path string) (map[string]string, error)) (string, error) {
